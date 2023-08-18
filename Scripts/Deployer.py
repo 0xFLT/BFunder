50ffd207e19c5e7dc55cf244656ff83865ca3089
FreeLunchTrading <64173645+0xFLT@users.noreply.github.com>
2023-08-18 15:30:46 -0500

    Update Deployer.py

--- Scripts/Deployer.py
+++ Scripts/Deployer.py
@@ -1,1 +1,11 @@
+from brownie import BFunder, DScripts, accounts
+from scripts.DScripts import get_account
+
+def deployFunder():
+    account = get_account()
+
+
+def main():
+    deployFunder()
+
 
